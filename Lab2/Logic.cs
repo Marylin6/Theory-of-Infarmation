61d5bf817cc4398ab2521bce15e598f53c00d5ee
Marylin6 <marylin06@yandex.by>
2026-03-12 14:55:42 +0300

    ready

---
+++ Lab2/Logic.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace lab2
+{
+    public class Logic
+    {
+        int[] register1, register2, register3;
+        public static bool IsValidKey(string key)
+        {
+            if (key.Length != 36)
+                return false;
+
+            foreach (char c in key)
+            {
+                if (c != '0' && c != '1')
+                    return false;
+            }
+
+            return true;
+        }
+        public static string BytesToBits(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+
+            foreach (byte b in data)
+            {
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+                sb.Append(" ");
+
+                //count++;
+
+                //if (count % 8 == 0)
+                //    sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+        public (byte[], string, string, string) Encrypt(string key1, string key2, string key3, byte[] data)
+        {
+            register1 = ParseKey(key1);
+            register2 = ParseKey(key2);
+            register3 = ParseKey(key3);
+
+            byte[] encrypted = new byte[data.Length];
+
+            StringBuilder keyStream1 = new StringBuilder();
+            StringBuilder keyStream2 = new StringBuilder();
+            StringBuilder keyStream3 = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                byte keyByte = 0;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    int x = NextBit(register1);
+                    int y = NextBit(register2);
+                    int z = NextBit(register3);
+
+                    keyStream1.Append(x);
+                    keyStream2.Append(y);
+                    keyStream3.Append(z);
+
+                    int geffe = (x & y) ^ ((1 - x) & z);
+
+                    keyByte = (byte)((keyByte << 1) | geffe);
+                }
+
+                encrypted[i] = (byte)(data[i] ^ keyByte);
+            }
+
+            return (encrypted, keyStream1.ToString(), keyStream2.ToString(), keyStream3.ToString());
+        }
+        private int[] ParseKey(string key)
+        {
+            int[] bits = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                bits[i] = key[i] - '0';
+            }
+
+            return bits;
+        }
+        private int NextBit(int[] register)
+        {
+            int newBit = register[0] ^ register[register.Length - 11];
+            int res = register[0];
+            for (int i = 0; i < register.Length - 1; i++)
+            {
+                register[i] = register[i + 1];
+            }
+            register[register.Length - 1] = newBit;
+            return res;
+        }
+    }
+}
---
+++ Lab2/resorses/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2
+{
+    public partial class MainWindow : Window
+    {
+        private string selectedFile;
+        private Logic logic = new Logic();
+        string keyStream1;
+        string keyStream2;
+        string keyStream3;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == true)
+            {
+                selectedFile = dialog.FileName;
+                FilePathBox.Text = selectedFile;
+            }
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string key1 = SeedBox1.Text;
+            string key2 = SeedBox2.Text;
+            string key3 = SeedBox3.Text;
+
+            if (!Logic.IsValidKey(key1) || !Logic.IsValidKey(key2) || !Logic.IsValidKey(key3))
+            {
+                MessageBox.Show("Ключ должен состоять из 36 символов 0 и 1.");
+                return;
+            }
+
+            byte[] data = File.ReadAllBytes(selectedFile);
+            var result = logic.Encrypt(key1, key2, key3, data);
+
+            byte[] encrypted = result.Item1;
+            keyStream1 = result.Item2;
+            keyStream2 = result.Item3;
+            keyStream3 = result.Item4;
+
+            OriginalBox.Text = Logic.BytesToBits(data);
+            EncryptedBox.Text = Logic.BytesToBits(encrypted);
+            File.WriteAllBytes(selectedFile + ".enc", encrypted);
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            string key1 = SeedBox1.Text;
+            string key2 = SeedBox2.Text;
+            string key3 = SeedBox3.Text;
+
+            byte[] data = File.ReadAllBytes(selectedFile);
+
+            var result = logic.Encrypt(key1, key2, key3, data);
+            byte[] decrypted = result.Item1;
+            keyStream1 = result.Item2;
+            keyStream2 = result.Item3;
+            keyStream3 = result.Item4;
+
+            string outputFile = selectedFile + ".dec";
+
+            File.WriteAllBytes(outputFile, decrypted);
+
+            OriginalBox.Text = Logic.BytesToBits(data);
+            EncryptedBox.Text = Logic.BytesToBits(decrypted);
+
+            Process.Start(new ProcessStartInfo(outputFile)
+            {UseShellExecute = true});
+        }
+        private void ShowKey1_Click(object sender, RoutedEventArgs e)
+        {
+            KeyStreamBox.Text = keyStream1;
+        }
+
+        private void ShowKey2_Click(object sender, RoutedEventArgs e)
+        {
+            KeyStreamBox.Text = keyStream2;
+        }
+
+        private void ShowKey3_Click(object sender, RoutedEventArgs e)
+        {
+            KeyStreamBox.Text = keyStream3;
+        }
+    }
+}
